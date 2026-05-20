3bb4ce3ef6e2846998b555ee36b5626c69c8c577
Gaby <105392025+Gaby4039@users.noreply.github.com>
2026-05-20 16:51:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * template_arduino_se.cpp
+ *
+ * Traditional embedded C/C++ programming style, without the setup() and loop() functions.
+ * Author: afonsomiguel
+ */
+
+// Including the main Arduino framework header is still necessary.
 #include <Arduino.h>
-
 #include "../../common/hal.h"
+#include "../../common/sm1.h"
+#include "../../common/sm2.h"
+#include "../../common/sm3.h"
+#include "../../common/sm4.h"
 
-#include "../../common/sm_t1.h"
-#include "../../common/sm_t2.h"
-#include "../../common/sm_t3.h"
-#include "../../common/sm_temp.h"
-
+/**
+ * @brief The program's main and single entry point.
+ */
 int main(void)
 {
+    // The call to init() REMAINS ESSENTIAL!
+    // It configures the hardware timers required for time-based functions like delay() and millis().
     init();
+	init_hal();
+	INIT(sm1.h);
+	INIT(sm2.h);
+	INIT(sm3.h);
+	INIT(sm4.h);	
 
-    init_hal();
+    // Set the built-in LED pin (LED_BUILTIN) as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
 
-    sm_t1_init();
-    sm_t2_init();
-    sm_t3_init();
-    sm_temp_init();
-
-    while (1)
+    // --- Main loop section ---
+    // Standard infinite loop for embedded systems. while(1) is a common idiom.
+    while (1) 
     {
-        sm_t1_exec();
-        sm_t2_exec();
-        sm_t3_exec();
-        sm_temp_exec();
+        // Turn the LED on
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        
+        // Turn the LED off
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
     }
 
-    return 0;
+    return 0; // This line is never reached.
 }
