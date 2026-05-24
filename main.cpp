43bf43449a25741f84cc0bac4c809084d05a93a2
Gaby <105392025+Gaby4039@users.noreply.github.com>
2026-05-24 20:10:58 -0300

    main_atualizada

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 
 // Including the main Arduino framework header is still necessary.
 #include <Arduino.h>
-#include "../../common/sm.h"
+
 #include "../../common/hal.h"
+
+#include "../../common/sm.h"
 #include "../../common/sm.h"
 #include "../../common/sm1.h"
 #include "../../common/sm2.h"
@@ -23,8 +25,12 @@ int main(void)
 {
     // The call to init() REMAINS ESSENTIAL!
     // It configures the hardware timers required for time-based functions like delay() and millis().
+    // Inicialização do Arduino
     init();
+    // Inicialização da HAL
 	init_hal();
+
+    // Inicialização máquinas de estado
 	
 	StateMachine sm1;
     INIT(sm1, sm1_init);
@@ -43,19 +49,10 @@ int main(void)
     // Standard infinite loop for embedded systems. while(1) is a common idiom.
     while (1) 
     {
-        // Turn the LED on
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        
-        // Turn the LED off
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-		
 		EXEC(sm1);
         EXEC(sm2);
         EXEC(sm3);
         EXEC(sm4);
-		
     }
 
     return 0; // This line is never reached.
